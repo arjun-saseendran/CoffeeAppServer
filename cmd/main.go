3646b629360d6ce764173f10e3b86622061ed75d
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-18 12:06:51 +0530

    add config server

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"coffee-app-server/internal/db"
+	"coffee-app-server/internal/order"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
-
-func main(){
+func main() {
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
 		log.Fatalf("failed to connect db %v", err)
 	}
 
-	
+	router := gin.Default()
+
+	orderService := order.NewOrderService(dbConnection)
+	orderHandler := order.NewOrderHandler(orderService)
+	orderHandler.RegisterEndPoints(router)
+
+	router.Run(":3000")
 }

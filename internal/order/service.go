53f4f072e928d3cd8ed1a359f3faf2eb1f0a46e6
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-18 11:15:47 +0530

    add order service

---
+++ internal/order/service.go
@@ -0,0 +1,67 @@
+package order
+
+import "gorm.io/gorm"
+
+type OrderService interface {
+	CreateOrder(orderData *InputCreateOrder) (*Order, error)
+	GetOrders() ([]Order, error)
+	GetOrder(id string) (*Order, error)
+	UpdateOrder(id string, orderData *InputUpdateOrder) (*Order, error)
+	DeleteOrder(id string) error
+}
+
+type orderService struct {
+	db *gorm.DB
+}
+
+func NewOrderService(db *gorm.DB) OrderService {
+	return &orderService{db}
+}
+
+func (os *orderService) CreateOrder(orderData *InputCreateOrder) (*Order, error) {
+	newOrder := &Order{Name: orderData.Name, CoffeeName: orderData.CoffeeName, Size: orderData.Size, Total: orderData.Total}
+	result := os.db.Create(newOrder)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return newOrder, nil
+
+}
+
+func (os *orderService) GetOrders() ([]Order, error) {
+	var orders []Order
+	result := os.db.Find(&orders)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return orders, nil
+}
+
+func (os *orderService) GetOrder(id string) (*Order, error) {
+	order := NewOrder()
+	result := os.db.First(&order, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return order, nil
+}
+
+func (os *orderService) UpdateOrder(id string, orderData *InputUpdateOrder) (*Order, error) {
+	updateOrder := NewOrder()
+	result := os.db.First(updateOrder, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	os.db.Model(&updateOrder).Updates(Order{Name: orderData.Name, CoffeeName: orderData.CoffeeName, Size: orderData.Size})
+	return updateOrder, nil
+}
+
+func (os *orderService) DeleteOrder(id string) error {
+	deleteOrder := NewOrder()
+	result := os.db.First(deleteOrder, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	os.db.Delete(deleteOrder)
+	return nil
+}
